1221e8b01aaa8e48cb5c28508b45956f1af71e4e
Olga Mamonova <shamrilo91@mail.ru>
2023-10-13 18:12:12 +0300

    добавляет основыне элементы формы

--- src/components/form.jsx
+++ src/components/form.jsx
@@ -4,14 +4,23 @@ class Form extends React.Component {
   state = {
     firstName: "",
     email: "",
+    message: "",
+    select: "",
+    subscription: false,
+    gender: "",
   };
 
   handleChange = (evt) => {
     this.setState({ [evt.target.name]: evt.target.value });
   };
 
+  handleCheckboxChange = (evt) => {
+    this.setState({ [evt.target.name]: evt.target.checked });
+  };
+
   render() {
-    const { firstName, email } = this.state;
+    const { firstName, email, message, select, subscription, gender } =
+      this.state;
     return (
       <div>
         <input
@@ -21,6 +30,7 @@ class Form extends React.Component {
           value={firstName}
           onChange={this.handleChange}
         />
+        <br />
         <input
           type="email"
           name="email"
@@ -28,6 +38,41 @@ class Form extends React.Component {
           value={email}
           onChange={this.handleChange}
         />
+        <br />
+        <textarea name="message" value={message} onChange={this.handleChange} />
+        <br />
+        <select name="select" value={select} onChange={this.handleChange}>
+          <option value="1">1</option>
+          <option value="2">2</option>
+          <option value="3">3</option>
+        </select>
+        <br />
+        <label>
+          <input
+            type="checkbox"
+            name="subscription"
+            checked={subscription}
+            onChange={this.handleCheckboxChange}
+          />
+          Subscription
+        </label>
+        <br />
+        <input
+          type="radio"
+          name="gender"
+          value="male"
+          onChange={this.handleChange}
+          checked={gender === "male"}
+        />
+        male
+        <input
+          type="radio"
+          name="gender"
+          value="female"
+          onChange={this.handleChange}
+          checked={gender === "female"}
+        />
+        female
       </div>
     );
   }
