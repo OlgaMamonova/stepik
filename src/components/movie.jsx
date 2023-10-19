3c0d0e184627eafb78be182858d6e26f8052ceec
Olga Mamonova <shamrilo91@mail.ru>
2023-10-19 11:42:17 +0300

    правит src

--- src/components/movie.jsx
+++ src/components/movie.jsx
@@ -10,7 +10,15 @@ function Movie(props) {
   return (
     <div className="card movie" id={id}>
       <div className="card-image waves-effect waves-block waves-light">
-        <img className="activator" src={poster} alt={title} />
+        {poster === "N/A" ? (
+          <img
+            className="activator"
+            src={`https://placehold.co/300x400?text=${title}`}
+            alt={title}
+          />
+        ) : (
+          <img className="activator" src={poster} alt={title} />
+        )}
       </div>
       <div className="card-content">
         <p className="card-title activator grey-text text-darken-4">{title}</p>
