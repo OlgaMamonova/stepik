423f09fd90a98f750f9cbd8cf8cc2a84468d1703
Olga Mamonova <shamrilo91@mail.ru>
2023-10-20 11:57:57 +0300

    add clicker

--- src/App.js
+++ src/App.js
@@ -1,5 +1,23 @@
+import React, { useState } from "react";
 function App() {
-  return <div className="App">hello from react</div>;
+  const [count, setCount] = useState(0);
+
+  const increment = () => {
+    setCount(count + 1);
+  };
+
+  const decrement = () => {
+    setCount(count - 1);
+  };
+  return (
+    <div className="App">
+      <button onClick={increment}>+</button>
+      <span style={{ display: "inline-block", margin: "0 0.5rem" }}>
+        {count}
+      </span>
+      <button onClick={decrement}>-</button>
+    </div>
+  );
 }
 
 export default App;
